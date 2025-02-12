36f67a1f20212174b4768f8456c84b0029ed8218
Hussein Hafid <61742496+Zakoroo@users.noreply.github.com>
2025-02-12 22:39:09 +0100

    Update tables.sql
    I made the schema a little bit simpler to deal with.  Now the Users and Chat rooms use name as primary key.

--- database/tables.sql
+++ database/tables.sql
@@ -1,34 +1,32 @@
 CREATE TABLE Users (
-    id SERIAL PRIMARY KEY,
-    name TEXT UNIQUE NOT NULL,
+    name TEXT PRIMARY KEY,
     email TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL
 );
 
-CREATE TABLE Chat_Rooms (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    chat_host INT REFERENCES Users
+CREATE TABLE ChatRooms (
+    name TEXT PRIMARY KEY,
+    chathost INT REFERENCES Users
 );
 
-CREATE TABLE Chat_Members (
-    user_id INT REFERENCES Users, 
-    chat_id INT REFERENCES Chat_Rooms,
-    PRIMARY KEY (user_id, chat_id)
+CREATE TABLE ChatMembers (
+    username INT REFERENCES Users, 
+    chatname INT REFERENCES ChatRooms,
+    PRIMARY KEY (username, chatname)
 );
 
 CREATE TABLE Messages (
-    user_id INT,
-    chat_id INT,
+    username INT,
+    chatname INT,
     timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     type TEXT CHECK (type IN ('text', 'image')),
-    text_msg TEXT,
-    image_url TEXT,
-    PRIMARY KEY (user_id, chat_id, timestamp),
-    FOREIGN KEY (user_id, chat_id) REFERENCES Chat_Members(user_id, chat_id),
+    textmsg TEXT,
+    imageurl TEXT,
+    PRIMARY KEY (username, chatname, timestamp),
+    FOREIGN KEY (username, chatname) REFERENCES ChatMembers(username, chatname),
     CHECK(
-        (type = 'text' AND text_msg IS NOT NULL AND image_url IS NULL) OR
-        (type = 'image' AND text_msg IS NULL AND image_url IS NOT NULL)
+        (type = 'text' AND textmsg IS NOT NULL AND imageurl IS NULL) OR
+        (type = 'image' AND textmsg IS NULL AND imageurl IS NOT NULL)
     )
 );
 
