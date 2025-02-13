1f3e6659549d2f77da0518beb1d76e515c3bc442
Zakoroo <maithemhussein27@gmail.com>
2025-02-13 20:11:10 +0100

    Refactor AccountHandler to include email in user registration and update authentication method to return User object

--- server/AccountHandler.java
+++ server/AccountHandler.java
@@ -1,4 +1,6 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class AccountHandler {
@@ -8,15 +10,33 @@ public class AccountHandler {
         this.conn = conn;
     }
 
-    public void registerUser(String username, String password) throws SQLException {
-        
+    public void registerUser(String username, String email, String password) throws SQLException {
+        String sql = "INSERT INTO Users VALUES(?,?,?)";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1, username);
+        ps.setString(2, email);
+        ps.setString(3, password);
+        ps.executeUpdate();
     }
 
-    public void authenticateUser(String username, String password) throws SQLException {
-
+    public User authenticateUser(String username, String password) throws SQLException {
+        String sql = "SELECT name, email FROM Users WHERE name = ? AND password = ?";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1, username);
+        ps.setString(2, password);
+        ResultSet rs = ps.executeQuery();
+        if(rs.next()) {
+            return new User(username, rs.getString("email"), password);
+        }
+        else {
+            return null;
+        }
     }
 
     public void deleteUser(String username, String password) throws SQLException {
-
+        String sql = "DELETE FROM Users WHERE name = ? AND password = ?";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1, username);
+        ps.setString(2, password);
     }
 }
