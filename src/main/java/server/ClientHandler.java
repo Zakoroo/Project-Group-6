24854f691cac8f0b86b8ec2d5366168603e68fbf
alzeine <mohamadalzein2000@gmail.com>
2025-02-25 23:20:16 +0100

    implemented quit_chat

--- src/main/java/server/ClientHandler.java
+++ src/main/java/server/ClientHandler.java
@@ -248,10 +248,33 @@ public class ClientHandler implements Runnable {
     
 
     private Container handleQuitChat(Object data) {
-        // TODO: implement this method
-        return null;
-
+        Map<String, String> params;
+    
+        try {
+            if (username == null) {
+                return new Container("error", "User not logged in!");
+            } else if (data instanceof String) {
+                params = parseData((String) data);
+                if (params.keySet().isEmpty()) {
+                    System.out.println("Parsed params are null");
+                    return new Container("error", "Invalid data!");
+                }
+    
+                boolean quit = chatHandler.quitChat(params.get("username"), params.get("chatname"));
+                if (quit) {
+                    return new Container("quit-chat-success", "User " + params.get("username") + " quit chat: " + params.get("chatname"));
+                } else {
+                    return new Container("error", "Failed to quit chat: " + params.get("chatname"));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new Container("error", "An error occurred: " + e.getMessage());
+        }
+    
+        return new Container("error", "Invalid data!");
     }
+    
 
     // Handle message sending and receiving
     private Container handleSendMessage(Object data) {
