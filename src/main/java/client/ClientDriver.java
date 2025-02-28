707ccc8fce5583203ba31f4b543154065b4ff1df
alzeine <mohamadalzein2000@gmail.com>
2025-02-28 17:47:13 +0100

    Added RequestHandler with all the functions to be called from the driver

--- src/main/java/client/ClientDriver.java
+++ src/main/java/client/ClientDriver.java
@@ -1,62 +1,67 @@
 package client;
-
-import java.io.*;
-import java.net.*;
 import shared.Container;
 
+import java.io.*;
+import java.net.Socket;
+import java.sql.Timestamp;
 
 public class ClientDriver {
     public static void main(String[] args) {
-        int PORT = 8005;
-        String serverAddress = "127.0.0.1";
+        String serverAddress = "localhost";
+        int port = 8005;
 
-        try (
-            Socket socket = new Socket(serverAddress, PORT);
-        ) {
-            System.out.println("Connected to the server");
-            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+        try (Socket socket = new Socket(serverAddress, port);
+             ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+             ObjectInputStream ois = new ObjectInputStream(socket.getInputStream())) {
 
-            Object responseObject;
-            Container responseContainer;
-    
-            //request signin
-            System.out.println("Signin request sent");
-            Container singinRequest = new Container("signin", "username=alzeine&password=87654321");
-            oos.writeObject(singinRequest);
-            oos.flush();
+            RequestHandler requestHandler = new RequestHandler(oos, ois);
 
-            // Signin response
-            System.out.println("Container received");
-            responseObject = ois.readObject();
-            responseContainer = (Container) responseObject;
-            System.out.println(responseContainer); 
+/*             // 1. Signup
+            System.out.println("Testing signup...");
+            requestHandler.signup("TestNick", "TestUser", "test@example.com", "password123");  */
 
-            //Create connect-chat request
-            System.out.println("connect-chat request sent");
-            Container connectchatrequest = new Container("connect-chat", "chatname=Intruders");
-            oos.writeObject(connectchatrequest);
-            oos.flush();
+            // 2. Signin
+            System.out.println("Testing signin...");
+            requestHandler.signin("TestUser", "password123"); 
+/* 
+             // 3. Create Chatroom
+            System.out.println("Testing create chat...");
+            requestHandler.createChat("TestChat"); */
+/* 
+            // 4. Join Chatroom
+            System.out.println("Testing join chat...");
+            requestHandler.joinChat("TestChat"); */
+/*
+            // 5. Connect to Chatroom
+            System.out.println("Testing connect chat...");
+            requestHandler.connectChat("TestChat");
 
-            //connect-chat response
-            responseObject = ois.readObject();
-            System.out.println("Container received");
-            responseContainer = (Container) responseObject;
-            System.out.println(responseContainer);
+            // 6. Find Chat
+            System.out.println("Testing find chat...");
+            requestHandler.findChat("Test");
 
-            //Create send-message request request
-            System.out.println("send message request sent");
-            Container sendmessagerequest = new Container("send-message", "hello world");
-            oos.writeObject(sendmessagerequest);
-            oos.flush();
+            // 7. Send Text Message
+            System.out.println("Testing send message (text)...");
+            requestHandler.sendMessage("Hello, this is a test message!");
 
-            // Create send-message response
-            responseObject = ois.readObject();
-            System.out.println("Container received");
-            responseContainer = (Container) responseObject;
-            System.out.println(responseContainer);
-          
-        } catch (Exception e) {
+            // 8. Send Image
+            System.out.println("Testing send message (image)..."); 
+            byte[] dummyImageData = new byte[]{1, 2, 3, 4, 5}; // Simulating binary data
+            requestHandler.sendMessage(dummyImageData);
+
+            // 9. Get Chat History
+            System.out.println("Testing get history...");
+            requestHandler.getHistory(new Timestamp(System.currentTimeMillis() - 36000000)); // 10 hours ago
+
+            // 10. Quit Chat
+            System.out.println("Testing quit chat...");
+            requestHandler.quitChat("TestChat");
+
+            // 11. Delete User
+            System.out.println("Testing delete user...");
+            requestHandler.deleteUser("password123"); */
+
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
